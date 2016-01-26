0e4c4707dd40f6c9268bccfd01f344cbe43d83f3
tikijian <timurt1988@gmail.com>
2016-01-26 13:49:44 +0300

    fix bait eating process

--- SDL_Snake/main.c
+++ SDL_Snake/main.c
@@ -1,7 +1,9 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <time.h>
 #include <SDL2/SDL.h>
+
 
 // directions
 #define UP 0
@@ -9,12 +11,15 @@
 #define LEFT 3
 #define RIGHT 4
 
+// Constants
 #define TILE_W 32
 #define TILE_H 32
 
+// Some game objects
 typedef struct
 {
-    int xPos, yPos;
+    ushort xPos, yPos;
+    ushort direction;
     
 } Block;
 
@@ -28,8 +33,11 @@ typedef struct
     int size;
     SnakeBlock *blocks;
     
-    unsigned short direction;
+    ushort direction;
 } SnakeHead;
+
+
+
 
 bool processEvents(SDL_Window *window, SnakeHead *snake)
 {
@@ -50,15 +58,23 @@ bool processEvents(SDL_Window *window, SnakeHead *snake)
                         is_running = false;
                         break;
                     case SDLK_w:
+                        if (snake->direction == DOWN)
+                            break;
                         snake->direction = UP;
                         break;
                     case SDLK_a:
+                        if (snake->direction == RIGHT)
+                            break;
                         snake->direction = LEFT;
                         break;
                     case SDLK_s:
+                        if (snake->direction == UP)
+                            break;
                         snake->direction = DOWN;
                         break;
                     case SDLK_d:
+                        if (snake->direction == LEFT)
+                            break;
                         snake->direction = RIGHT;
                         break;
                     default:
@@ -69,10 +85,7 @@ bool processEvents(SDL_Window *window, SnakeHead *snake)
                 is_running = false;
                 break;
         }
-        
-        
     }
-    
     return is_running;
 }
 
@@ -81,10 +94,13 @@ void update(SnakeHead *snake)
     for (int i = snake->size - 1; i >= 1; i--) {
         snake->blocks[i].xPos = snake->blocks[i-1].xPos;
         snake->blocks[i].yPos = snake->blocks[i-1].yPos;
+        snake->blocks[i].direction = snake->blocks[i-1].direction;
     }
     
     snake->blocks[0].xPos = snake->xPos;
     snake->blocks[0].yPos = snake->yPos;
+    snake->blocks[0].direction = snake->direction;
+
     
     switch (snake->direction) {
         case UP:
@@ -103,6 +119,47 @@ void update(SnakeHead *snake)
             break;
     }
 }
+
+bool isBaitEaten(SnakeHead *snake, Bait *bait)
+{
+    return snake->xPos == bait->xPos && snake->yPos == bait->yPos;
+}
+
+void updateSnakeBody(SnakeHead *snake, Bait *bait)
+{
+    if(! isBaitEaten(snake, bait) ) return;
+
+    // generate new Bait
+    bait->xPos = rand() % 20;
+    bait->yPos = rand() % 15;
+    
+    // add new block to the end of snake body
+    int i = snake->size;
+    snake->blocks[i].direction = snake->blocks[i-1].direction;
+    snake->blocks[i].xPos = snake->blocks[i-1].xPos;
+    snake->blocks[i].yPos = snake->blocks[i-1].yPos;
+    
+    // calculate correct coordinates offset, depending on direction
+    switch (snake->blocks[i].direction) {
+        case UP:
+            snake->blocks[i].yPos += 1;
+            break;
+        case DOWN:
+            snake->blocks[i].yPos -= 1;
+            break;
+        case LEFT:
+            snake->blocks[i].yPos += 1;
+            break;
+        case RIGHT:
+            snake->blocks[i].yPos -= 1;
+            break;
+        default:
+            break;
+    }
+
+    snake->size++;
+}
+
 
 void renderField(SDL_Renderer *gfx)
 {
@@ -140,7 +197,18 @@ void renderSnake(SDL_Renderer *gfx, SnakeHead *snake)
     }
 }
 
+void renderBait(SDL_Renderer *gfx, Bait *bait)
+{
+    SDL_Rect rect = {bait->xPos * TILE_W, bait->yPos * TILE_H, TILE_H, TILE_W};
+    SDL_SetRenderDrawColor(gfx, 255, 255, 0, 255);
+    SDL_RenderFillRect(gfx, &rect);
+}
+
+// ============================== Main ===============================
+
 int main(int argc, const char * argv[]) {
+    srand((unsigned int) time(NULL)); // init random seed
+    
     bool is_running = true;
     SnakeHead snake;
     snake.xPos = 10;
@@ -156,6 +224,8 @@ int main(int argc, const char * argv[]) {
     };
     snake.blocks = blocks;
     
+    Bait bait = {rand() % 20, rand() % 15};
+    
     SDL_Init(SDL_INIT_VIDEO);
     
     SDL_Window *window = NULL;
@@ -163,7 +233,6 @@ int main(int argc, const char * argv[]) {
     
     window = SDL_CreateWindow("My SDL Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    
     
     while (is_running) {
         is_running = processEvents(window, &snake);
@@ -174,9 +243,12 @@ int main(int argc, const char * argv[]) {
         
         // Render
         renderField(renderer);
+        renderBait(renderer, &bait);
         renderSnake(renderer, &snake);
         
         SDL_RenderPresent(renderer);
+        
+        updateSnakeBody(&snake, &bait);
         
         SDL_Delay(300);
     }
