d6394a0459dd13f6a11a1a42be645aecdb9fd029
tikijian <timurt1988@gmail.com>
2016-01-26 22:08:19 +0300

    New graphics. some refactoring. screen bounds teleport

--- SDL_Snake/main.c
+++ SDL_Snake/main.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <time.h>
 #include <SDL2/SDL.h>
+#include <SDL2_image/SDL_Image.h>
 
 
 // directions
@@ -15,29 +16,72 @@
 #define TILE_W 32
 #define TILE_H 32
 
+#define FIELD_W 20
+#define FIELD_H 15
+
 // Some game objects
 typedef struct
 {
     ushort xPos, yPos;
     ushort direction;
-    
-} Block;
+} SnakeBlock;
 
-typedef Block SnakeBlock;
-typedef Block Bait;
+
+typedef struct
+{
+    ushort xPos, yPos;
+    SDL_Texture *drakeTex;
+} Bait;
 
 typedef struct
 {
     int x,y;
     int xPos, yPos;
     int size;
-    SnakeBlock *blocks;
-    
     ushort direction;
+    
+    SnakeBlock *blocks;
+    SDL_Texture *headTex;
 } SnakeHead;
 
+SnakeHead initSnake(SDL_Renderer* gfx)
+{
+    SnakeHead snake;
+    snake.xPos = 10;
+    snake.yPos = 9;
+    snake.size = 4;
+    snake.direction = UP;
+    
+    // Load image
+    SDL_Surface *yobaSurface = IMG_Load("yoba.png");
+    if (yobaSurface == NULL) {
+        printf("Can't find yoba.png!");
+        SDL_Quit();
+        exit(1);
+    }
+    // set texture
+    snake.headTex = SDL_CreateTextureFromSurface(gfx, yobaSurface);
+    SDL_FreeSurface(yobaSurface);
+    
+    return snake;
+}
 
+Bait initBait(SDL_Renderer* gfx)
+{
+    Bait bait = {rand() % FIELD_W, rand() % FIELD_H};
+    // Load image
+    SDL_Surface *drakeSurface = IMG_Load("drakeface.png");
+    if (drakeSurface == NULL) {
+        printf("Can't find drakeface.png!");
+        SDL_Quit();
+        exit(1);
+    }
+    // set texture
+    bait.drakeTex = SDL_CreateTextureFromSurface(gfx, drakeSurface);
+    SDL_FreeSurface(drakeSurface);
 
+    return bait;
+}
 
 bool processEvents(SDL_Window *window, SnakeHead *snake)
 {
@@ -57,22 +101,22 @@ bool processEvents(SDL_Window *window, SnakeHead *snake)
                     case SDLK_ESCAPE:
                         is_running = false;
                         break;
-                    case SDLK_w:
+                    case SDLK_UP:
                         if (snake->direction == DOWN)
                             break;
                         snake->direction = UP;
                         break;
-                    case SDLK_a:
+                    case SDLK_LEFT:
                         if (snake->direction == RIGHT)
                             break;
                         snake->direction = LEFT;
                         break;
-                    case SDLK_s:
+                    case SDLK_DOWN:
                         if (snake->direction == UP)
                             break;
                         snake->direction = DOWN;
                         break;
-                    case SDLK_d:
+                    case SDLK_RIGHT:
                         if (snake->direction == LEFT)
                             break;
                         snake->direction = RIGHT;
@@ -100,7 +144,6 @@ void update(SnakeHead *snake)
     snake->blocks[0].xPos = snake->xPos;
     snake->blocks[0].yPos = snake->yPos;
     snake->blocks[0].direction = snake->direction;
-
     
     switch (snake->direction) {
         case UP:
@@ -118,6 +161,17 @@ void update(SnakeHead *snake)
         default:
             break;
     }
+    
+    // teleport to opposite side of screen (easy mode for my son)
+    if(snake->xPos > FIELD_W)
+        snake->xPos = 0;
+    else if(snake->xPos < 0)
+        snake->xPos = FIELD_W;
+    
+    if(snake->yPos > FIELD_H)
+        snake->yPos = 0;
+    else if (snake->yPos < 0)
+        snake->yPos = FIELD_H;
 }
 
 bool isBaitEaten(SnakeHead *snake, Bait *bait)
@@ -130,8 +184,8 @@ void updateSnakeBody(SnakeHead *snake, Bait *bait)
     if(! isBaitEaten(snake, bait) ) return;
 
     // generate new Bait
-    bait->xPos = rand() % 20;
-    bait->yPos = rand() % 15;
+    bait->xPos = rand() % FIELD_W;
+    bait->yPos = rand() % FIELD_H;
     
     // add new block to the end of snake body
     int i = snake->size;
@@ -169,13 +223,13 @@ void renderField(SDL_Renderer *gfx)
     SDL_Rect rect = {0, 0, TILE_H, TILE_W};
     
     for (int i = 0; i < 300; i++) {
-        if (i != 0 && i % 20 == 0){
+        if (i != 0 && i % FIELD_W == 0){
             x = 0;
             y += TILE_H;
         }
         rect.x = x;
         rect.y = y;
-        SDL_RenderDrawRect(gfx, &rect);
+        SDL_RenderFillRect(gfx, &rect);
         
         x = x + TILE_W;
     }
@@ -186,8 +240,7 @@ void renderSnake(SDL_Renderer *gfx, SnakeHead *snake)
     snake->x = snake->xPos * TILE_W;
     snake->y = snake->yPos * TILE_H;
     SDL_Rect rect = {snake->x, snake->y, TILE_H, TILE_W};
-    SDL_SetRenderDrawColor(gfx, 200, 0, 0, 255);
-    SDL_RenderFillRect(gfx, &rect);
+    SDL_RenderCopy(gfx, snake->headTex, NULL, &rect);
     
     SDL_SetRenderDrawColor(gfx, 255, 255, 255, 255);
     for (int i = 0; i < snake->size; i++) {
@@ -200,31 +253,13 @@ void renderSnake(SDL_Renderer *gfx, SnakeHead *snake)
 void renderBait(SDL_Renderer *gfx, Bait *bait)
 {
     SDL_Rect rect = {bait->xPos * TILE_W, bait->yPos * TILE_H, TILE_H, TILE_W};
-    SDL_SetRenderDrawColor(gfx, 255, 255, 0, 255);
-    SDL_RenderFillRect(gfx, &rect);
+    SDL_RenderCopy(gfx, bait->drakeTex, NULL, &rect);
 }
 
 // ============================== Main ===============================
 
 int main(int argc, const char * argv[]) {
     srand((unsigned int) time(NULL)); // init random seed
-    
-    bool is_running = true;
-    SnakeHead snake;
-    snake.xPos = 10;
-    snake.yPos = 9;
-    snake.size = 4;
-    snake.direction = UP;
-    
-    SnakeBlock blocks[50] = {
-        {10, snake.yPos+1},
-        {10, snake.yPos+2},
-        {10, snake.yPos+3},
-        {10, snake.yPos+4}
-    };
-    snake.blocks = blocks;
-    
-    Bait bait = {rand() % 20, rand() % 15};
     
     SDL_Init(SDL_INIT_VIDEO);
     
@@ -233,6 +268,18 @@ int main(int argc, const char * argv[]) {
     
     window = SDL_CreateWindow("My SDL Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    
+    bool is_running = true;
+    SnakeHead snake = initSnake(renderer);
+    SnakeBlock blocks[50] = {
+        {10, snake.yPos+1},
+        {10, snake.yPos+2},
+        {10, snake.yPos+3},
+        {10, snake.yPos+4}
+    };
+    snake.blocks = blocks;
+    
+    Bait bait = initBait(renderer);
     
     while (is_running) {
         is_running = processEvents(window, &snake);
@@ -255,6 +302,8 @@ int main(int argc, const char * argv[]) {
     
     // Shutdown
     
+    SDL_DestroyTexture(snake.headTex);
+    SDL_DestroyTexture(bait.drakeTex);
     SDL_DestroyRenderer(renderer);
     
     SDL_Quit();
